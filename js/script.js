98bc70619b45cb7925e0e8530aefe76e709da294
Fa <taunus95@gmail.com>
2022-08-08 17:01:01 +0200

    Praktikum1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
-"use strict";
+let numberOfFilms = prompt("Wie viele Filme haben sie schon gesehen?", "");
 
-const isChecked = false, 
-    isClosed = false;
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
 
-console.log(isChecked || !isClosed);
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false
+};
+
+let movie1 = prompt("Welchen Film haben zu zuletzt gesehen", "");
+let note1 = prompt("Wie bewerten Sie den letzten Film (von 0 bis 10, dezimal)", ""); 
+let movie2 = prompt("Welchen Film zu als zweitletzten gesehen", "");
+let note2 = prompt("Wie bewerten Sie den vorletzten Film (von 0 bis 10, dezimal)", ""); 
+
+personalMovieDB.movies[movie1] = note1;
+personalMovieDB.movies[movie2] = note2;
+
+console.log(personalMovieDB);
