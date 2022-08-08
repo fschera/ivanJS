5c6c70ed5e9a6d1402635394cab579ad966bd986
Fa <taunus95@gmail.com>
2022-08-08 17:05:07 +0200

    let --> const

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let numberOfFilms = prompt("Wie viele Filme haben sie schon gesehen?", "");
+const numberOfFilms = prompt("Wie viele Filme haben sie schon gesehen?", "");
 
-let personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
     movies: {
 
@@ -12,10 +12,10 @@ let personalMovieDB = {
     privat: false
 };
 
-let movie1 = prompt("Welchen Film haben zu zuletzt gesehen", "");
-let note1 = prompt("Wie bewerten Sie den letzten Film (von 0 bis 10, dezimal)", ""); 
-let movie2 = prompt("Welchen Film zu als zweitletzten gesehen", "");
-let note2 = prompt("Wie bewerten Sie den vorletzten Film (von 0 bis 10, dezimal)", ""); 
+const movie1 = prompt("Welchen Film haben zu zuletzt gesehen", "");
+const note1 = prompt("Wie bewerten Sie den letzten Film (von 0 bis 10, dezimal)", ""); 
+const movie2 = prompt("Welchen Film zu als zweitletzten gesehen", "");
+const note2 = prompt("Wie bewerten Sie den vorletzten Film (von 0 bis 10, dezimal)", ""); 
 
 personalMovieDB.movies[movie1] = note1;
 personalMovieDB.movies[movie2] = note2;
